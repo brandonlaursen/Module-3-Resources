cd81e33ebc8c3584971e5895be62e7da8377ab01
Maica <maicasantos90@gmail.com>
2024-02-27 14:25:54 -0800

    static assests

--- Week8/Day2/04-serve-static-assets/server.js
+++ Week8/Day2/04-serve-static-assets/server.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  // res.end('Hey Maica')
+
+  const fileContents = fs.readFileSync('./index.html','utf-8');
+  // console.log(fileContents);
+  res.statusCode = 200;
+  res.setHeader("Content-Type","text/html");
+  return res.end(fileContents);
+
 });
 
 const port = 5000;
