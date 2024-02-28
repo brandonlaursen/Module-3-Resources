8ec3a507e3957767cee50cf41483d3a784db2992
brandon <brandonlaursen95@yahoo.com>
2024-02-28 16:44:37 -0500

    solution

--- Week8/Day3/05-fetch-long-practice/fetch-requests.js
+++ Week8/Day3/05-fetch-long-practice/fetch-requests.js
@@ -1,20 +1,115 @@
+
+
+
 /*
-Make fetch requests in the browser for each of the following phases.
-Paste your code for fetch requests here once you finish each phase.
+ ! Make fetch requests in the browser for each of the following tasks.
+ can also do in browser
+ const fetch = require("node-fetch");
+
+Paste your code for fetch requests here once you finish each task.
 */
 
-/* ============================== Phase 1 ============================== */
+/* =============== 1. Print the status code of the response =============== */
 
-// Your code here
+//!!START
+// using Promise chain (.then)
+fetch("/products")
+  .then(res => console.log(res.status));
+
+// using async/await
+(async function() {
+  const res = await fetch("/products");
+  console.log(res.status);
+})();
+//!!END
 
 
 
-/* ============================== Phase 2 ============================== */
-
-// Your code here
-
+// * SIMPLEST WAY
+const response =  await fetch('/products')
 
 
-/* ============================== Phase 3 ============================== */
+/* ====== 2. Print true if the status of the response was successful ====== */
 
-// Your code here
+//!!START
+// using Promise chain (.then)
+fetch("/products")
+  .then(res => console.log(res.ok));
+
+// using async/await
+(async function() {
+  const res = await fetch("/products");
+  console.log(res.ok);
+})();
+//!!END
+
+
+// * SIMPLEST WAY
+// if(response.status === 200) console.log(true)
+if(response.ok) console.log(response.ok)
+
+/* =================== 3. Print the Content-Type Header =================== */
+//!!START
+// using Promise chain (.then)
+fetch("/products")
+  .then(res => console.log(res.headers.get('Content-Type')));
+
+// using async/await
+(async function() {
+  const res = await fetch("/products");
+  res.headers.get('Content-Type');
+})();
+//!!END
+
+
+// * SIMPLEST WAY
+console.log(response.headers.get('Content-Type'))
+
+
+/* ============== 4. Print the body of the response as text =============== */
+
+
+// console.log(response.text())
+
+
+
+// below is the more practical format on how we would be using fetch
+// this practice was mainly to show us how to retrieve data from our response object
+
+// we only await 3 things when making our requests
+
+/*
+1. const res = await fetch('/someURL')
+2. const body = await res.text()
+or
+3. const body = await res.json() <--- we will see this later when working with web apis.
+
+*/
+
+//!!START
+// using Promise chain (.then)
+fetch("/products")
+  .then(res => res.text())
+  .then(resBody => console.log(resBody));
+
+// using async/await
+(async function() {
+  const res = await fetch("/products");
+  const resBody = await res.text();
+  console.log(resBody);
+})();
+//!!END
+
+// * SIMPLEST WAY
+const getProducts = async () => {
+    const result = await fetch('/products')
+
+    if(result.ok){
+        const data = await result.text()
+
+        console.log(data)
+    }
+}
+
+getProducts()
+//
--- Week8/Day3/05-fetch/fetch-requests.js
+++ Week8/Day3/05-fetch/fetch-requests.js
@@ -1,26 +1,87 @@
+
+
 /*
-Make fetch requests in the browser for each of the following tasks.
-Paste your code for fetch requests here once you finish each task.
+
+    Fetch
+     - The Fetch API provides an interface for fetching resources (including across the network)
+    * The fetch() method can optionally accept a second parameter, an init object that allows you to control a number of different settings:
+    * url = "", data = {}
+    * can specify data of the request
+    * if request is a post we can specify the headers and the body
+    * if request is a get we only need the url
+    ! assessment will be like this, setting up the data endpoints for the fetch, no building out routes
+
 */
 
-/* =============== 1. Print the status code of the response =============== */
 
-// Your code here
+// * get all dogs
+export function getAllDogs() {
+  // Your code here
+  const url = "/dogs";
 
+  // const options = {
+  //     method: 'GET',
+  //     headers: {
+  //         "Content-Type": 'text/html'
+  //     }
+  // }
 
+  return fetch(url);
+}
 
-/* ====== 2. Print true if the status of the response was successful ====== */
+getAllDogs();
 
-// Your code here
+// * get second dog
+export function getDogNumberTwo() {
+  // Your code here
+  return fetch("/dogs/2");
+}
 
+// * post new dog
+export function postNewDog() {
 
+  // * posting a new dog using just the the button
+  const urlparams = new URLSearchParams({ name: "little bear", age: 14 });
+  return fetch("/dogs", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: urlparams,
+  });
 
-/* =================== 3. Print the Content-Type Header =================== */
+  // return fetch('/dogs', {
+  //     method: 'POST',
+  //     headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded"
+  //     },
+  //     body: new URLSearchParams({
+  //         "name":"hachi roku",
+  //         "age": 86
+  //     })
+  // })
+}
 
-// Your code here
+// * posting a dog using the form taking in arguments
+export function postNewDogV2(name, age) {
+  // Your code here
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      name,
+      age,
+    }),
+  });
+}
 
+// * delete dog
+export function deleteDog(id) {
 
-
-/* ============== 4. Print the body of the response as text =============== */
-
-// Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: {
+      AUTH: "ckyut5wau0000jyv5bsrud90y",
+    },
+  });
+}
