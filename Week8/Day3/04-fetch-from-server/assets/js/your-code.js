1c3b6cb20ebfaf0358f3f89ccd072d4b74dd04b6
brandon <brandonlaursen95@yahoo.com>
2024-02-28 16:19:42 -0500

    we love dogs

--- Week8/Day3/04-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/04-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,47 @@
 export function getAllDogs() {
-  // Your code here
+  return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-  // Your code here
+  return fetch("/dogs/2", {
+    method: "GET",
+  });
 }
 
 export function postNewDog() {
   // Your code here
+
+  const urlSearchParam = new URLSearchParams({ name: "little bear", age: 15 });
+
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: urlSearchParam,
+  });
 }
 
 export function postNewDogV2(name, age) {
-  // Your code here
+  // const urlSearchParam = new URLSearchParams({ name, age });
+
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ name, age }),
+  });
 }
 
 export function deleteDog(id) {
   // Your code here
+
+
+  return fetch(`/dogs/${id}/delete`, {
+    method: 'POST',
+    headers: {
+      AUTH: 'ckyut5wau0000jyv5bsrud90y'
+    }
+  })
 }
