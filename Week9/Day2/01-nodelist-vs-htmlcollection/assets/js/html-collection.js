4eaa67a98b06ca72a85d2fbe416619137f83495a
brandon <brandonlaursen95@yahoo.com>
2024-03-05 13:46:21 -0500

    solutions

--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,11 +1,23 @@
+
+
+
+
+
+
+
 export default () => {
+
   const bodyChildElements = document.body.children; // HTMLCollection [div]
   // console.log(bodyChildElements);
 
   const div = bodyChildElements[0];
 
   const divChildElements = div.children; // HTMLCollection [span]
+  // console.log(divChildElements);
+
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  // console.log(helloWorld);
+
   const span = divChildElements[0]; // <span>Yes!</span>
   // debugger
 };
--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
@@ -1,11 +1,21 @@
 export default () => {
   const bodyChildNodes = document.body.childNodes; // NodeList [text, div, text]
-  // console.log(bodyChildNodes);
+  console.log(bodyChildNodes);
 
   const div = bodyChildNodes[1]; // NOT bodyChildNodes[0]
 
+  // console.log(div);
+
   const divChildNodes = div.childNodes; // NodeList [text, span, text]
+
+  console.log(divChildNodes);
+
+
   const helloWorld = divChildNodes[0].textContent; // Hello World!\n
+
+  // console.log(helloWorld);
+
   const span = divChildNodes[1]; // <span>Yes!</span>
+  // console.log(span);
   // debugger
 };
