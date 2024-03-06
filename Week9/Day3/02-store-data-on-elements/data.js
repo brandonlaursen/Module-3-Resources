f465575d00bde6ecc7c7d678a6fd970fc1796f9e
brandon <brandonlaursen95@yahoo.com>
2024-03-06 13:47:05 -0500

    practice 2

--- Week9/Day3/02-store-data-on-elements/data.js
+++ Week9/Day3/02-store-data-on-elements/data.js
@@ -1,2 +1,27 @@
 // Your code here
 
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  const addToList = document.getElementById("add");
+
+  addToList.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const shoppingList = document.getElementById("shopping-list");
+
+    const name = document.getElementById("name");
+
+    const type = document.getElementById(`type`);
+
+    const li = document.createElement("li");
+
+    li.innerText = name.value;
+
+    li.dataset.type = type.value;
+
+    shoppingList.appendChild(li);
+
+    name.value = "";
+    type.value = "dairy";
+  });
+});
