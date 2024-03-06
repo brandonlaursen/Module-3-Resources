333c3599efa4bb505f8c49d082d77cafa52a66ae
brandon <brandonlaursen95@yahoo.com>
2024-03-06 13:23:12 -0500

    practice 1

--- Week9/Day3/01-add-remove-event-listeners/events.js
+++ Week9/Day3/01-add-remove-event-listeners/events.js
@@ -1,1 +1,65 @@
-// Your code here
+window.addEventListener("DOMContentLoaded", () => {
+  // alert('dom')
+
+  const redInput = document.getElementById("red-input");
+
+  function changeToRed (e) {
+    let value = e.target.value.trim().toLowerCase();
+
+    if (value === "red") {
+      redInput.style.backgroundColor = "red";
+    } else {
+      redInput.style.backgroundColor = "transparent";
+    }
+  };
+
+  // redInput.addEventListener("input", (e) => {
+  //   let value = e.target.value.trim().toLowerCase();
+
+  //   if (value === "red") {
+  //     redInput.style.backgroundColor = "red";
+  //   } else {
+  //     redInput.style.backgroundColor = "transparent";
+  //   }
+  // });
+
+    redInput.addEventListener("input", changeToRed);
+
+  const addItem = document.getElementById("add-item");
+
+  const ul = document.querySelector("ul");
+
+  const addToList = (e) => {
+    const input = document.getElementById("list-add");
+
+    const value = input.value;
+
+    const li = document.createElement("li");
+
+    li.innerText = value;
+
+    ul.appendChild(li);
+
+    input.value = "";
+  };
+
+  addItem.addEventListener("click", addToList);
+
+  const colorSelector = document.getElementById("color-select");
+
+  const changeColor = (e) => {
+    const section = document.getElementById("section-3");
+
+    section.style.backgroundColor = e.target.value;
+  };
+
+  colorSelector.addEventListener("input", changeColor);
+
+  const removeListeners = document.getElementById("remove-listeners");
+
+  removeListeners.addEventListener("click", () => {
+    redInput.removeEventListener("input", changeToRed);
+    addItem.removeEventListener("click", addToList);
+    colorSelector.removeEventListener("input", changeColor);
+  });
+});
